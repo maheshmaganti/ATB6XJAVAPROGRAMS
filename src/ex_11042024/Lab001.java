49e8e39d3f8e8d7a4236d17b36b15823f33d8530
Mahesh Maganti <maheshsmart0@gmail.com>
2024-04-11 17:55:57 +0530

    first commit

---
+++ src/ex_11042024/Lab001.java
@@ -0,0 +1,21 @@
+package ex_11042024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Maga");
+        System.out.print("mj");
+        System.out.print("mkjh");
+// single line comments
+
+        /**
+         *  double lien comemnts
+         *
+         *
+         */
+
+
+
+    }
+}
---
+++ src/ex_11042024/Lab002.java
@@ -0,0 +1,21 @@
+package ex_11042024;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Lab002 {
+
+
+
+    public static void main(String[] args) {
+
+        int age = 68;
+
+        age = 45;
+
+        age = age+45;
+
+        System.out.println(age);
+
+    }
+
+}
